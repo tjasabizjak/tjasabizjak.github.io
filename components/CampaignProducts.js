a9ece71710620e1ad13be970bb3a26be3bfb2b7e
Tjaša Bizjak <tjasa11@gmail.com>
2023-09-23 10:54:41 +0200

    Initial commit

---
+++ components/CampaignProducts.js
@@ -0,0 +1,208 @@
+app.component("campaign-products", {
+  template:
+    /*html*/
+    `<div class="products-container">
+      <div class="products">
+        <div class="gender-btn">
+          <button class="gender-woman-btn">ZA ŽENSKE</button>
+          <button class="gender-man-btn">ZA MOŠKE</button>
+        </div>
+        <div class="group-btns">
+          <a href="https://www.ceneje.si/L3/1032/zenska-oblacila/zensko-spodnje-perilo-pizame/zenski-modrcki-nedrcki?Area=Web" target="_blank"><button class="group-btn">Modrčki / nedrčki</button></a>
+          <a href="https://www.ceneje.si/L3/1034/zenska-oblacila/zensko-spodnje-perilo-pizame/zenske-pizame?Area=Web" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="https://www.ceneje.si/L3/742/modni-dodatki/nakit/prstani?Area=Web" target="_blank"><button class="group-btn">Prstani</button></a>
+          <a href="https://www.ceneje.si/L3/952/lepota-in-zdravje/parfumerija/parfumi?Area=Web" target="_blank"><button class="group-btn">Parfumi</button></a>
+          <a href="https://www.ceneje.si/L3/1085/modni-dodatki/torbe-in-torbice/modne-torbice?Area=Web" target="_blank"><button class="group-btn">Modne torbice</button></a>
+        </div>
+        <div class="select-products-container">
+          <img
+          src="https://omara.cdn-cnj.si/static/frontend/live/sl/img/valentines/2022/balloon.webp"
+          width="96"
+          height="252"
+          class="img-floating-gift"
+          />
+          <div class="select-products-container">
+            <div class="select-products">
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+              <div class="select-product">
+                <img src="https://s3.eu-central-1.amazonaws.com/cnj-img/images/Qn/QnWsagSNiQrs" />
+                <h2>33,90 €</h2>
+                <h3>kopalnica-rea.si</h3>
+                <p>Ženski kopalni plačš siv medvedek</p>
+              </div>
+          </div>
+        </div>
+        <div class="group-btns">
+          <a href="https://www.ceneje.si/L3/1034/zenska-oblacila/zensko-spodnje-perilo-pizame/zenske-pizame?Area=Web" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="https://www.ceneje.si/L3/1034/zenska-oblacila/zensko-spodnje-perilo-pizame/zenske-pizame?Area=Web" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+          <a href="" target="_blank"><button class="group-btn">Pižame</button></a>
+        </div>
+        <div class="more-gifts">
+          <img
+          src="https://omara.cdn-cnj.si/static/frontend/live/sl/img/valentines/2022/gift.webp"
+          width="227"
+          height="114"
+          class="img-gift"
+          />
+          <button class="more-gifts-btn">Pssst, imamo tudi drugačna darila...</button>
+          <img
+          src="https://omara.cdn-cnj.si/static/frontend/live/sl/img/valentines/last-tail.png"
+          width="312"
+          height="89"
+          class="img-heart"
+          />
+        </div>
+      </div>
+    </div>`,
+});
